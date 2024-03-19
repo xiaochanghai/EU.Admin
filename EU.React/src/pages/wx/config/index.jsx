59d509f215a1f65a1d0156c4192c420fa8613148
xiaochanghai <xiaochanghai@foxmail.com>
2024-03-19 22:15:30 +0800

    删除无效代码

--- EU.React/src/pages/wx/config/index.jsx
+++ EU.React/src/pages/wx/config/index.jsx
@@ -1,59 +1,52 @@
-// eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react';
 import { connect } from 'umi';
-// eslint-disable-next-line no-unused-vars
-
-// eslint-disable-next-line no-unused-vars
 import TableList from './components/TableList'
-// eslint-disable-next-line no-unused-vars
 import FormPage from './components/FormPage';
 
 let me;
 class wxconfig extends Component {
-    constructor(props) {
-        super(props);
-        me = this;
-        me.state = {
-            current: <TableList />
-        };
+  constructor(props) {
+    super(props);
+    me = this;
+    me.state = {
+      current: <TableList />
+    };
+  }
+
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'wxconfig/setTableStatus',
+      payload: {},
+    })
+  }
+
+  componentWillMount() {
+    const tempRowId = localStorage.getItem("tempRowId");
+    if (tempRowId) {
+      localStorage.removeItem('tempRowId');
+      me.setState({ current: <FormPage Id={tempRowId} IsView={true} /> })
     }
+  }
 
-    componentWillUnmount() {
-        const { dispatch } = this.props;
-        dispatch({
-            type: 'wxconfig/setTableStatus',
-            payload: {},
-        })
-    }
+  static changePage(current) {
+    me.setState({ current })
+  }
 
-    componentWillMount() {
-        const tempRowId = localStorage.getItem("tempRowId");
-        if (tempRowId) {
-            localStorage.removeItem('tempRowId');
-            me.setState({ current: <FormPage Id={tempRowId} IsView={true} /> })
-        }
-    }
+  render() {
+    const { current } = this.state;
+    const pageComponent = current;
+    return (
+      <>
+        {pageComponent}
+      </>
+    )
 
-    static changePage(current) {
-        me.setState({current})
-    }
-
-    render() {
-        const { current } = this.state;
-        const pageComponent = current;
-        return (
-            <>
-
-                    {pageComponent}
-
-            </>
-        )
-
-    }
+  }
 }
 
 // eslint-disable-next-line no-shadow
 export default connect(({ wxconfig }) => ({
-    wxconfig
-    // loading: loading.effects['dashboardAndanalysis/fetch'],
+  wxconfig
+  // loading: loading.effects['dashboardAndanalysis/fetch'],
 }))(wxconfig);
